e367cb5345d3f3bb403efa36b1566dbdb0d43fd9
dreambigatall <halidmahammed.16.hm@gmail.com>
2024-12-21 16:41:59 +0300

    we work on the account , login, singup, signout functionality

--- src/features/authentication/useUser.js
+++ src/features/authentication/useUser.js
@@ -7,5 +7,5 @@ export function useUser() {
     queryFn: getCurrentUser,
   });
 
-  return { isLoading, user, isAuthenticated: user?.role === "authenticated" };
+  return { isLoading, user, isAuthenticated: user?.role === "authenticated" };//in this code the following part work /**isAuthenticated: user?.role === "authenticated"**/when the user.role is equivalent with the "authenicated" then it become true the isAuthnticated value then based on that we can make the user ethir redirected to the home page or to the login page agin
 }
